b6b99361463945cd1605a777ce167a6cbebabd46
0m4rP <omar17penagos@gmail.com>
2023-06-25 19:41:52 -0500

    finalizacion de los punto 5-6-7

--- punto-5-6-7/index.php
+++ punto-5-6-7/index.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../styles/general-styles.css">
+    <link rel="stylesheet" href="./view/styles/table.css">
+    <title>Punto 5-6-7</title>
 </head>
 <body>
     <h1>Proyecto Base de datos</h1>
-    <?php include "./service/connection.php" ?> 
+    <?php include "./view/components/table.php" ?> 
 </body>
 </html>
--- punto-5-6-7/service/connection.php
+++ punto-5-6-7/service/connection.php
@@ -1,7 +1,3 @@
 <?php 
     const CONNECTION = new mysqli("localhost", "root", "", "mi_proyecto");
-    echo "paso aquí";
-    $query = mysqli_query(CONNECTION, "SELECT * FROM usuarios");
-
-    print_r(mysqli_fetch_all($query, MYSQLI_ASSOC));
 ?>
